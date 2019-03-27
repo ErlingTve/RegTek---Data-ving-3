7fcdecb512a6d333c6e4772e853f837bc0ef35c9
Erling Tveter <erlingtv@stud.ntnu.no>
2019-03-27 14:25:45 +0100

    Prøver meg på oppgave 2 c)

--- Dataøving3/motor.m
+++ Dataøving3/motor.m
@@ -1,6 +1,12 @@
 s = tf('s');
 h_u = (K_T)/((J*L_a)*(s^3) + (J*R_a + B*L_a)*(s^2) + (R_a*B + K_T*K_v)*s);
-K_p = 1;
+K_p = 18.6;
 h_r = K_p;
 h_0 = h_u * h_r;
+
+margin(h_0);
+hold();
+
+N = 1/(1+h_0);
+bode(N);
 
