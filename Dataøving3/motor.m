f02dce847aeb122e66e4fa0e34c44e0eca3e5f1b
Erling Tveter <erlingtv@stud.ntnu.no>
2019-03-28 13:32:03 +0100

    Ferdig oppgave 3

--- Dataøving3/motor.m
+++ Dataøving3/motor.m
@@ -1,5 +1,8 @@
+%Oppgave 1
 s = tf('s');
 h_u = (K_T)/((J*L_a)*(s^3) + (J*R_a + B*L_a)*(s^2) + (R_a*B + K_T*K_v)*s);
+
+%Oppgave 2
 Kp = 18.6;
 h_r = Kp;
 h_0 = h_u * h_r;
@@ -12,3 +15,8 @@ bode(N);
 [mag, phase, wout] = bode(N, {650, 800});
 AbsN_max = max(mag);
 AbsN_maxInDB = mag2db(AbsN_max);
+
+%Oppgave 3
+dPsi = 2;
+Ts_grense = 2*dPsi/W_c;
+Ts = Ts_grense - 0.0001;
