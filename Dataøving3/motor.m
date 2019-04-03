799f1f0f23f09ed7485b5270b98c3b20ae87c511
Erling Tveter <erlingtv@stud.ntnu.no>
2019-04-03 14:55:49 +0200

    shit

--- Dataøving3/motor.m
+++ Dataøving3/motor.m
@@ -20,3 +20,24 @@ AbsN_maxInDB = mag2db(AbsN_max);
 dPsi = 2;
 Ts_grense = 2*dPsi/W_c;
 Ts = Ts_grense - 0.0001;
+
+%Oppgave 4
+% Kpk=168.23;
+Kp = 100;
+a=0.1;
+Td = 0.19/(W_c*sqrt(a));
+h_r = Kp*(1+Td*s)/(1+a*Td*s);
+h_0 = h_r * h_u;
+margin(h_0);
+
+%Oppgave 5
+% Tk = 3.4*10^(-3);
+% Ti= 20 * Tk;
+% %Ti = 999999999999999;
+% Td = 0.12 * Tk;
+% %Td=0;
+% h_r = Kp*(1+Ti*s)*(1+Td*s)/(Ti*s*(1+a*Td*s));
+% h_0 = h_r * h_u;
+% margin(h_0);
+
+
